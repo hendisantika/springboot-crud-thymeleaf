f664a2627a1e76a32c1b54f80c34c5b59141e749
Hendi Santika <hendisantika@gmail.com>
2018-11-18 05:38:10 +0700

    #3 - Add User entity

---
+++ src/main/java/com/hendisantika/springbootcrudthymeleaf/entity/User.java
@@ -0,0 +1,43 @@
+package com.hendisantika.springbootcrudthymeleaf.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-crud-thymeleaf
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/11/18
+ * Time: 05.36
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Entity
+@Data
+@ToString
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
